bf136bd449c12bf4b32c4beb1e2ab24019deec45
MooseCowBear <82609260+MooseCowBear@users.noreply.github.com>
2023-07-05 07:33:25 -0400

    add script, adjust css to toggle light and dark theme with button press

--- script.js
+++ script.js
@@ -1,21 +1,22 @@
-const menu_button = document.querySelector(".menu_button");
+const menuButton = document.querySelector(".menu_button");
+menuButton.addEventListener("click", toggleMenu);
 
-//want to toggle "hidden" class from 1. nav links, 2. close icon div, 3. hamburger icon div
-menu_button.addEventListener("click", toggleMenu);
-
-function toggleMenu() {
-  const elems = document.querySelectorAll(".toggle_visibility");
-  elems.forEach(elem => elem.classList.toggle("hidden"));
-}
-
-//when clicking any link inside the menu, want the menu to close, only when in mobile - ie. if the menu was open
+//when clicking any link inside the menu, want the menu to close
 const menu = document.querySelector(".nav_links");
-
 menu.addEventListener("click", (event) => {
   if (event.target.tagName.toLowerCase() === 'a') {
     closeMenu();
   }
 });
+
+//toggle dark/light mode
+const themeButton = document.querySelector(".theme");
+themeButton.addEventListener("click", toggleTheme);
+
+function toggleMenu() {
+  const elems = document.querySelectorAll(".toggle_visibility");
+  elems.forEach(elem => elem.classList.toggle("hidden"));
+}
 
 function closeMenu() {
   if (!menu.classList.contains("hidden")) {
@@ -23,7 +24,7 @@ function closeMenu() {
   }
 }
 
-//close menu with "x" or if clicking outside - do i want this too?
-
-
-//toggle dark/light mode
+function toggleTheme() {
+  document.body.classList.toggle("light-mode");
+  document.body.classList.toggle("dark-mode");
+}
